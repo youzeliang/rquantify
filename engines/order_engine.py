c08a2f765578925455363e8beea3ae0f4c4f7b3f
youze <youzel@126.com>
2023-08-17 01:49:02 +0800

    update order

--- engines/order_engine.py
+++ engines/order_engine.py
@@ -1,3 +1,8 @@
+import json
+import requests
+
+from utils import logger
+
 order_url = 'http://127.0.0.1:12344/order/insert'
 position_url = 'http://127.0.0.1:12344/query/position'
 account_url = 'http://127.0.0.1:12344/query/account'
@@ -15,4 +20,23 @@ header = {
 class OrderEngine:
 
     def __init__(self):
-        pass
+        self.logger = logger.get_logger(log_dir=logger.LOG_FILE)
+
+    def z_cancel(self, order_id: str):
+        """
+        取消订单
+        :param order_id:
+        :return:
+        """
+        data = {
+            "order_id": order_id
+        }
+
+        try:
+            res = json.loads(requests.post(cancel_url, headers=header, json=data).text)
+            error_id = res['error_id']
+            error_msg = res['error_msg']
+            if error_id != '0' or error_msg != '':
+                self.logger.error(error_msg)
+        except Exception as e:
+            self.logger.error(e)
