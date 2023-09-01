d5834b63c7c159f476487b106aa3779aead18590
liangyouze <liangyouze@tal.com>
2023-09-01 10:22:07 +0800

    add economic data

--- engines/notice_engine.py
+++ engines/notice_engine.py
@@ -1,5 +1,8 @@
 import os, sys
 import socket
+import requests
+import time
+import datetime
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
@@ -31,7 +34,32 @@ class Notice:
             logger.get_logger(log_dir=logger.ERROR_LOG_FILE).error(e)
             return False
 
+    def economic_data(self):
+        current_date = datetime.datetime.now()
+        formatted_date = current_date.strftime('%m/%d')
+        url = f'https://cdn-rili.jin10.com/web_data/{current_date.year}/daily/{formatted_date}/economics.json?t=1693451525225'
+        response = requests.get(url)
+        data = response.json()
+        for i in data:
+            if i['country'] in ['中国', '美国'] or int(i['star']) >= 3:
+                continue
+            ignore = [
+                '天然气', '芝加哥'
+            ]
 
-if __name__ == '__main__':
-    s = Notice()
-    s.check_port()
+            flag = False
+            for x in ignore:
+                if x in str(i['name']):
+                    flag = True
+                    break
+            if flag:
+                continue
+
+            current_timestamp = int(time.time())
+            if i['pub_time_unix'] > current_timestamp:
+                continue
+
+            s = i['country'] + ' ' + i['name'] + ' ' + '  前值:' + str(i['previous']) + ' ' + '  预期值: ' + str(
+                i['consensus']) + '  实际值: ' + str(i['actual']) + '  ' + str(i['star']) + '星'
+
+            return s
