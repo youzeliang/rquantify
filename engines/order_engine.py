6a4031d1cb8b7d86157e338a01c2f6cfc8de8833
liangyouze <youzel@126.com>
2023-09-14 18:14:42 +0800

    add order list

--- engines/order_engine.py
+++ engines/order_engine.py
@@ -21,9 +21,26 @@ header = {
 
 class OrderEngine:
 
-    def __init__(self):
+    def __init__(self, quote_ctx: OpenQuoteContext, trade_ctx: OpenSecTradeContext):
         self.default_logger = logger.Logger().logger
+        self.quote_ctx = quote_ctx
+        self.trade_ctx = trade_ctx
+        self.trd_env = TrdEnv.REAL
 
+    def get_order_list(self):
+        """
+        查询多日订单,默认当日
+        :return:
+        """
+        # https://openapi.futunn.com/futu-api-doc/trade/get-order-list.html，每3s 才能请求一次
+        ret_code, order_list_data = self.trade_ctx.order_list_query(order_id="", trd_env=self.trd_env,
+                                                                    start=time.strftime("%Y-%m-%d", time.localtime()),
+                                                                    refresh_cache=False)
+
+        if ret_code != RET_OK:
+            self.default_logger.error(f"Cannot acquire order list {order_list_data}")
+            return None
+        return order_list_data
 
     def place_order(self, code: str, side: str, exchange: str, type: str, price: str, qty: int):
         """
